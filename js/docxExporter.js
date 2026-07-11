933c1e8714bb9c802c9e95904d3f7590ca687931
Ayan Khan <ayankhan964825@example.com>
2026-07-11 13:37:28 +0530

    UI: Tighten DOCX line spacing to 1.15
    Reverts the strict 1.6 line spacing back to a more standard 1.15 to
    prevent the exported Word/PDF documents from looking too empty.

--- js/docxExporter.js
+++ js/docxExporter.js
@@ -177,7 +177,7 @@ const DocxExporter = (() => {
             '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
             '<w:styles xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">' +
             '<w:style w:type="paragraph" w:default="1" w:styleId="Normal"><w:name w:val="Normal"/>' +
-            '<w:pPr><w:spacing w:after="200" w:line="384" w:lineRule="auto"/><w:jc w:val="both"/></w:pPr>' +
+            '<w:pPr><w:spacing w:after="160" w:line="276" w:lineRule="auto"/><w:jc w:val="both"/></w:pPr>' +
             '<w:rPr><w:rFonts w:ascii="Times New Roman" w:hAnsi="Times New Roman" w:cs="Times New Roman"/><w:sz w:val="24"/><w:szCs w:val="24"/></w:rPr></w:style>' +
             [1,2,3,4,5,6].map(n => {
                 // H1: 14pt (28 half-pts), H2-H6: 12pt (24 half-pts) per research paper rules
